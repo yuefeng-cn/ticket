7175e4a5094b8e4f8346d6086dccb600915f27a4
yuefeng.niu <yuefeng.niu@outlook.com>
2024-09-12 16:34:03 +0800

    add nextUI; dark theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
-import {
-  UserButton,
-} from "@clerk/nextjs";
+import { UserButton } from "@clerk/nextjs";
+import { Button } from "@nextui-org/button";
+import { div } from "framer-motion/client";
+import { ThemeSwitcher } from "./components/ThemeSwitcher";
 export default function Home() {
   return (
-    <UserButton />
+    <div>
+      <ThemeSwitcher />
+      <UserButton />
+      <Button>Click me</Button>
+    </div>
   );
 }
